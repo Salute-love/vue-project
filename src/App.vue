f4e0c77badac116809755af524529805198ceeb6
TXH <TXH@163.com>
2018-11-19 21:27:16 +0800

    项目初始化, 完成头部和底部导航

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app-container">
+    <!-- 头部 -->
+    <mt-header fixed title="Vue CMS"></mt-header>
+
+    <!-- 底部tabBar -->
+    <nav class="mui-bar mui-bar-tab">
+			<router-link class="mui-tab-item mui-active" to="/home">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">会员</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/shopcar">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"></span>
+				<span class="mui-tab-label">购物车</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">搜索</span>
+			</router-link>
+		</nav>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="less">
+
+</style>
