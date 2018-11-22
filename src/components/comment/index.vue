a869dccf0796c25aefa4a32c8080c5eeef01d528
TXH <TXH@163.com>
2018-11-22 21:45:19 +0800

    完成了新闻资讯和资讯详情页面

---
+++ src/components/comment/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="comment-container">
+        <h2>发表评论</h2>
+        <hr>
+        <textarea placeholder="请输入您要评论的内容！最多输入120字" maxlength="120"></textarea>
+        <mt-button type="primary" size="large">发表评论</mt-button>
+        <div class="comment-list">
+            <div class="comment-item" v-for="(item, index) in commentList" :key="index">
+                <div class="comment-title">第{{index + 1 }}楼&nbsp;&nbsp;用户:{{item.user_name}}&nbsp;&nbsp;发表时间:{{item.add_time | dateFormat}}</div>
+                <div class="comment-content">
+                    {{item.content || "这个人着实有点懒"}}
+                </div>
+            </div>
+        </div>
+        <mt-button type="danger" size="large" plain>加载更多</mt-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["id"],
+        data() {
+            return {
+                pageIndex: 1,
+                commentList: [] 
+            };
+        },
+        created() {
+            this.getComments();
+        },
+        methods: {
+            getComments(){
+                this.$http.get("/api/getcomments/" + this.id+ "?pageindex=" + this.pageIndex ).then(result => {
+                        // console.log(result)
+                        if(result.body.status === 0){
+                            this.commentList = this.commentList.concat(result.body.message);
+                        }else {
+                            Toast('加载失败，请重试！');
+                        }
+                    
+                })
+            }
+        }
+    }
+</script>
+
+<style lang="less">
+    .comment-container {
+        .comment-list {
+            .comment-item {
+                margin: 5px 0;
+                .comment-title {
+                    font-size: 13px;
+                    background-color: #ccc;
+                }
+                .comment-content {
+                    font-size: 13px;
+                }
+            }
+        }
+    }
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import homeComponent from '../pages/home/'
 import memberComponent from '../pages/member/'
 import shopcarComponent from '../pages/shopcar/'
 import searchComponent from '../pages/search/'
+import newsListComponent from '../pages/newsList/'
+import newsInfoComponent from '../pages/newsInfo/'
 
 Vue.use(Router)
 
@@ -15,6 +17,8 @@ export default new Router({
     { path: '/member', component: memberComponent },
     { path: '/shopcar', component: shopcarComponent },
     { path: '/search', component: searchComponent },
+    { path: '/home/newsList', component: newsListComponent },
+    { path: '/home/newsInfo/:id', component: newsInfoComponent },
   ],
   linkActiveClass: 'mui-active'
 })
